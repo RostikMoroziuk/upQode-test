451dfc2da43ce5ed7fecc71a1e507503180fb617
RostikMoroziuk <rmorozyukua@gmail.com>
2017-06-15 21:50:41 +0300

    Add navigation and banner

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require("gulp");
 var scss = require("gulp-scss");
 var concat = require("gulp-concat");
-var browserSync = require("browser-sync");
 //build
 var minifyCSS = require("gulp-csso");
 var rename = require("gulp-rename");
@@ -10,48 +9,29 @@ var uglify = require("gulp-uglify");
 
 //scss files compile
 gulp.task("css", function () {
-  return gulp.src("scss/*.scss")
+  return gulp.src("src/scss/*.scss")
     .pipe(scss())
     .pipe(concat("all.css"))
-    .pipe(gulp.dest("build/css"))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+    .pipe(gulp.dest("build/css"));
 });
 
 
 //html file add to build
 gulp.task("html", function () {
-  return gulp.src(config.html)
-    .pipe(gulp.dest("build"))
-    .pipe(reload({
-      stream: true
-    }));
+  return gulp.src("src/index.html")
+    .pipe(gulp.dest("build"));
 });
 
 //scripts add to build
 gulp.task("scripts", function () {
-  return gulp.src("js")
-    .pipe(gulp.dest("build/js"))
-    .pipe(reload({
-      stream: true
-    }));
-});
-
-//synchronized with browser
-gulp.task("browser-sync", function () {
-  browserSync.init({
-    server: {
-      baseDir: "build/"
-    },
-    port: 8080
-  });
+  return gulp.src("src/js")
+    .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("watch", function () {
   gulp.watch("scss/*.scss", ["css"]);
-  gulp.watch("*.html", ["html"]);
+  gulp.watch("src/*.html", ["html"]);
   gulp.watch("js/*.js", ["scripts"]);
 });
 
-gulp.task("default", ["watch", "browser-sync"]);
+gulp.task("default", ["watch"]);
