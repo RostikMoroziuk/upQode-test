49cce3abab973db06bd3e73e7d5b1567ab68cff7
RostikMoroziuk <rmorozyukua@gmail.com>
2017-06-16 10:11:36 +0300

    Add bower for hosting on gh-page

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,40 @@
 $(function () {
+  //Add Google Maps to page
   function initMap() {
+    //Lviv coords
     var coords = {
       lat: 49.834050,
       lng: 24.008120
     };
+    //Add on page
     new google.maps.Map(document.querySelector(".js-map"), {
       zoom: 16,
       center: coords
     });
   }
 
+  function menu() {
+    //Dropdown menu second level on hover
+    $('ul.nav > li.dropdown').hover(function () { //hover on
+      $(this).find('.dropdown-menu').eq(0).slideDown();
+    }, function () { //hover out
+      $(this).find('.dropdown-menu').eq(0).slideUp();
+    });
+
+    //Dropdown menu third level on hover
+    var parentDropdownWidth = $('.nav > .dropdown > .dropdown-menu').eq(0) //2nd level menu
+      .width();
+    $('.dropdown-menu > li.dropdown').hover(function () { //hover on
+      $(this).find('.dropdown-menu').eq(0).show().animate({ //right slide
+        left: parentDropdownWidth
+      });
+    }, function () { //hover out
+      $(this).find('.dropdown-menu').eq(0).animate({ //right slide
+        left: 0
+      }).hide()
+    });
+  }
+
   window.initMap = initMap;
+  menu();
 });
