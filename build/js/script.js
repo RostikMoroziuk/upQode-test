6231ce4a87a2f8d7e671404f9a89b1a43f4d80b0
RostikMoroziuk <rmorozyukua@gmail.com>
2017-06-16 23:55:40 +0300

    Styled all sections

--- build/js/script.js
+++ build/js/script.js
@@ -35,20 +35,63 @@ $(function () {
     });
 
     //card on why choose us hover 
-    $(".card").hover(function() {//hover on
-      console.log($(this).find(".card--content-hover"));
-      $(this).find(".card--content-hover").css({height: "149px"});
-    },
-    function() {//hover out
-      $(this).find(".card--content-hover").css({height: "0"});
+    $(".card").hover(function () { //hover on
+        console.log($(this).find(".card--content-hover"));
+        $(this).find(".card--content-hover").css({
+          height: "149px"
+        });
+      },
+      function () { //hover out
+        $(this).find(".card--content-hover").css({
+          height: "0"
+        });
+      })
+    
+    //Change page on our product
+    $(".dots--dot").on("click", function() {
+      //target dot
+      var dot = $(this);
+      if(dot.find(".dots--dot-active").length > 0) {//if click on current page
+        return;
+      }
+      var pageIndex = dot.attr("data-index");
+      //change active page
+      $(".products--content-active").fadeOut(200, function() {
+        $(".products--content-active").removeClass("products--content-active");
+        $(".products--content").eq(pageIndex).fadeIn(200).addClass("products--content-active");
+      });
+
+      $(".dots--dot-active").removeClass("dots--dot-active");
+      dot.addClass("dots--dot-active");
     })
   }
 
+  //Draw svg filter for header in our products section
+  function drawFilter() {
+    var TRIANGLE_WIDTH = 138;
+    var HEADER_HEIGHT = 75; //in percentage
+    //Parent is products block
+    var parentWidth = $(".products").width();
+    var path = $(".svg--field path"); //drawing path for polygon
+
+    var triangleStart = parseInt(parentWidth / 2 - TRIANGLE_WIDTH / 2); //in pixels
+    triangleStart = ((triangleStart / parentWidth) * 100).toFixed(2); //in percentage
+
+    var triangleEnd = 100 - triangleStart;
+    console.log(triangleStart, triangleEnd);
+    path.attr("d", "M0,0 L 100,0 100 " + HEADER_HEIGHT + " " + triangleEnd + "," + HEADER_HEIGHT
+    + " 50,100 " + triangleStart + "," + HEADER_HEIGHT + " " + 0 + "," +  HEADER_HEIGHT + "Z");
+
+  }
+
+  window.initMap = initMap;
   //Bootstrap carousel
+
+  drawFilter()
   $('.carousel').carousel({
     interval: 2000000
   })
 
-  window.initMap = initMap;
   init();
+
 });
